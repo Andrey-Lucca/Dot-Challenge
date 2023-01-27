638749d2c5bb6f633adb6de35731c7342af16513
Andrey-Lucca <122308400+Andrey-Lucca@users.noreply.github.com>
2023-01-27 17:34:11 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*Desenvolva uma aplicação em que:
+
+- o usuário pode clicar em qualquer lugar da página.
+- deve-se renderizar um pequeno círculo na posição clicada.
+- a cada clique, mantém-se os círculos já criados e renderiza-se um novo.
+- crie duas funcionalidades para a aplicação:
+    - desfazer (undo)
+    - refazer (redo)
+*/
 function App() {
   
   const [list, setList] = useState([])
